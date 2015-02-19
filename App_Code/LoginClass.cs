c251d6ed0609e22c7e68e97326228b382bcdb9ce
jcrimp <jenny.crimp@gmail.com>
2015-02-19 11:59:18 -0800

    Initial commit

---
+++ App_Code/LoginClass.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// This class takes in the user name and password,
+/// retrieves information from the database
+/// adn then hashes the password and key to
+/// see if it matches the database hash
+/// </summary>
+public class LoginClass
+{
+    //class level variables-fields
+    private string pass;
+    private string username;
+    private int seed;
+    private byte[] dbHash;
+    private int key;
+    private byte[] newHash;
+	
+    //constructor logic takes in password and username
+    public LoginClass(string pass, string username)
+    {
+        this.pass = pass;
+        this.username = username;
+    }
+
+    //get the user info from database
+    private void GetUserInfo()
+    {
+        //declare the ADO Entities
+        ShowTrackerEntities showTrackerDb = new ShowTrackerEntities();
+        //query the fields
+        var info = from c in showTrackerDb.VenueLogins
+                   where c.VenueLoginUserName.Equals(username)
+                   select new { c.VenueLoginHashed, c.VenueLoginRandom, c.VenueKey };//FanLoginRandom is the random number
+
+        //loop through the results and assign the
+        //values to the field variables
+        foreach (var u in info)
+        {
+            seed = (int)u.VenueLoginRandom;
+            dbHash = u.VenueLoginHashed;
+            key = (int)u.VenueKey;//cast it to an int
+        }
+    }
+
+    private void GetNewHash()
+    {
+        //get the new hash
+        PasswordHash h = new PasswordHash();
+        newHash = h.HashIt(pass, seed.ToString());
+    }
+
+    private bool CompareHash()
+    {
+        //compare the hashes
+        bool goodLogin = false;
+
+        //if the hash doesn't exist
+        //because not a valid user
+        //the return will be false
+
+        if(dbHash != null)
+        {
+            //if the hashes do match, return true
+            if(newHash.SequenceEqual(dbHash))//SequenceEqual compares arrays
+                goodLogin = true;
+        }
+
+        return goodLogin;
+    }
+
+    public int ValidateLogin()
+    {
+        //call the methods
+        GetUserInfo();
+        GetNewHash();
+        bool result = CompareHash();//find out if it is false
+
+        //if the result is not true
+        //set the key to 0
+        if (!result)
+            key = 0;
+
+        return key;
+    }
+
+}
---
+++ App_Code/VenueRegistrationService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "VenueRegistrationService" in code, svc and config file together.
+public class VenueRegistrationService : IVenueRegistrationService
+{
+    ShowTrackerEntities showTrackerDb = new ShowTrackerEntities();
+
+    public bool RegisterVenue(Venue v, VenueLogin vl)
+    {
+        bool result = true;
+
+        try
+        {
+            PasswordHash phash = new PasswordHash();
+            KeyCode kcode = new KeyCode();
+            int key = kcode.GetKeyCode();
+            byte[] hash = phash.HashIt(vl.VenueLoginPasswordPlain, key.ToString());
+
+            Venue venue = new Venue();
+            venue.VenueName = v.VenueName;
+            venue.VenueEmail = v.VenueEmail;
+            venue.VenueWebPage = v.VenueWebPage;
+            venue.VenuePhone = v.VenuePhone;
+            venue.VenueAddress = v.VenueAddress;
+            venue.VenueCity = v.VenueCity;
+            venue.VenueState = v.VenueState;
+            venue.VenueZipCode = v.VenueZipCode;
+            venue.VenueAgeRestriction = v.VenueAgeRestriction;
+            venue.VenueDateAdded = DateTime.Now;
+            showTrackerDb.Venues.Add(venue);
+
+            VenueLogin venueLogin = new VenueLogin();
+            venueLogin.Venue = venue;
+            venueLogin.VenueLoginUserName = vl.VenueLoginUserName;
+            venueLogin.VenueLoginPasswordPlain = vl.VenueLoginPasswordPlain;
+            venueLogin.VenueLoginHashed = hash;
+            venueLogin.VenueLoginDateAdded = DateTime.Now;
+            venueLogin.VenueLoginRandom = key;
+            showTrackerDb.VenueLogins.Add(venueLogin);
+
+            showTrackerDb.SaveChanges();
+        }
+
+        catch
+        {
+            result = false;
+        }
+
+            return result;
+    }
+
+    public int Login(string password, string username)
+    {
+        LoginClass login = new LoginClass(password, username);
+        int key = login.ValidateLogin();
+        return key;
+    }
+}
